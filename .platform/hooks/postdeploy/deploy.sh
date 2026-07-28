0d948755ab505a3825c8b01c4f9c9494f7446bc9
Muhammad Rafay <muhammadrafay7655@gmail.com>
2026-07-28 21:11:21 +0500

    Fixed db creds

--- .platform/hooks/postdeploy/deploy.sh
+++ .platform/hooks/postdeploy/deploy.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+set -e
 
-  echo "Link storage/app/public with efs.."
-  sudo ln -snf /mnt/efs/wp-content /var/app/current/wp-content
-  echo "Fix ownership /var/app/current/wp-content"
-  sudo chown -R webapp:webapp /var/app/current/wp-content
-# sudo chmod -R 777 /var/app/current/wp-content/uploads
-sudo chown -R webapp:webapp /mnt/efs/wp-content
+echo "Preparing EFS..."
+
+mkdir -p /mnt/efs
+
+# First deployment only
+if [ ! -d /mnt/efs/wp-content ]; then
+    echo "First deployment - copying wp-content to EFS"
+    cp -a /var/app/current/wp-content /mnt/efs/
+fi
+
+echo "Replacing wp-content with EFS symlink"
+
+rm -rf /var/app/current/wp-content
+
+ln -s /mnt/efs/wp-content /var/app/current/wp-content
+
+chown -R webapp:webapp /mnt/efs/wp-content
